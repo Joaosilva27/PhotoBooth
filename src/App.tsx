c4249a892675f5ceb0e47c272f4b5e4050897420
João Silva <joaosilva7875@gmail.com>
2025-02-25 10:07:23 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div className="">
+        <h1>PhotoBooth</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
