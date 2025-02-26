1a500f8e216a1762f95cf0730a1c3907877586fa
João Silva <joaosilva7875@gmail.com>
2025-02-26 20:49:01 +0100

    working webcam container & tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 import "./App.css";
+import Webcam from "react-webcam";
 
 function App() {
   return (
     <div>
       <div className="">
-        <h1>PhotoBooth</h1>
+        <h1 className="text-3xl font-bold">PhotoBooth</h1>
+        <div className="border-solid border-4 border-black rounded-lg">
+          <Webcam height={600} width={600} />
+        </div>
       </div>
     </div>
   );
